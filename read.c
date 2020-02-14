0258281b38ddd00a237c3d3fc986f8f3752680e0
Alexander Polleti <alexander.polleti@siemens.com>
2020-02-14 14:25:08 +0100

    enhance help message

--- read.c
+++ read.c
@@ -13,62 +13,71 @@ typedef uint16_t register_type_t;
 #define ELMENT_SEPERATOR "\t"
 
 int print_manual(FILE *file, char const *argv0) {
-  (void)argv0;
-  return fputs(
-      "modbus_read_register is a simple modbus client that will try to read "
-      "registers and quit.\n"
-      "modbus_read_register is running on libmodbus "
-      "Version " LIBMODBUS_VERSION_STRING
-      "\n"
-      "Usage: modbus_read_register\n",
-      file);
+    (void)argv0;
+    return fputs(
+        "modbus_read_register is a simple modbus client that will try to read "
+        "registers and quit.\n"
+        "modbus_read_register is running on libmodbus "
+        "Version " LIBMODBUS_VERSION_STRING
+        "\n"
+        "Usage: modbus_read_register -h 192.168.0.1 -p 602 -u 1 -s 40000 -n 125 -x\n"
+        "will read 125 registers starting from address 40000 with from 192.168.0.1:602 with unit id 1\n\n"
+        "-p --port host port, default is 502\n"
+        "-h --host server host name defaults to localhost\n"
+        "-u --unit_id used unit id to use, defaults to 0xff as suggested by ModbusTCP\n"
+        "-s --address first modbus register address to read from\n"
+        "-n number of registers to read\n"
+        "-v verbose mode\n"
+        "-x --hex hexadecimal output\n"
+        "-H --help print this help message\n",
+        file);
 }
 
 static int print(register_type_t const *data, size_t n_data) {
 #if (defined READ_COILS) || (defined READ_INPUT_BITS)
-  char const *format_string = "%d\t";
+    char const *format_string = "%d\t";
 #elif (defined READ_HOLDING_REGISTERS) || (defined READ_INPUT_REGISTERS)
-  char const *format_string = use_hex ? ("%" PRIx16 "\t") : ("%" PRIu16 "\t");
+    char const *format_string = use_hex ? ("%" PRIx16 "\t") : ("%" PRIu16 "\t");
 #endif
-  for (size_t i = 0; i < n_data; ++i) {
-    int result = printf(format_string, data[i]);
-    if (result <= 0) return EXIT_FAILURE;
-  }
-  return EXIT_SUCCESS;
+    for (size_t i = 0; i < n_data; ++i) {
+        int result = printf(format_string, data[i]);
+        if (result <= 0) return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
 }
 
 int handle_request(void) {
-  uint16_t n_read_registers = 0;
+    uint16_t n_read_registers = 0;
 
-  do {
-    uint16_t to_read_registers = n_registers - n_read_registers;
-    if (to_read_registers > MODBUS_MAX_READ_REGISTERS) {
-      to_read_registers = MODBUS_MAX_READ_REGISTERS;
-    }
-    n_read_registers += to_read_registers;
+    do {
+        uint16_t to_read_registers = n_registers - n_read_registers;
+        if (to_read_registers > MODBUS_MAX_READ_REGISTERS) {
+            to_read_registers = MODBUS_MAX_READ_REGISTERS;
+        }
+        n_read_registers += to_read_registers;
 
-    register_type_t read_regs[MODBUS_MAX_READ_REGISTERS];
-    int read_res =
+        register_type_t read_regs[MODBUS_MAX_READ_REGISTERS];
+        int read_res =
 #if defined READ_COILS
-        modbus_read_bits(ctx, addr, to_read_registers, read_regs);
+            modbus_read_bits(ctx, addr, to_read_registers, read_regs);
 #elif defined READ_INPUT_BITS
-        modbus_read_input_bits(ctx, addr, to_read_registers, read_regs);
+            modbus_read_input_bits(ctx, addr, to_read_registers, read_regs);
 #elif defined READ_HOLDING_REGISTERS
-        modbus_read_registers(ctx, addr, to_read_registers, read_regs);
+            modbus_read_registers(ctx, addr, to_read_registers, read_regs);
 #elif defined READ_INPUT_REGISTERS
-        modbus_read_input_registers(ctx, addr, to_read_registers, read_regs);
+            modbus_read_input_registers(ctx, addr, to_read_registers, read_regs);
 #else
 #error define read function
 #endif
 
-    if (read_res < 0) {
-      (void)fprintf(stderr, "Error: %s\n", modbus_strerror(errno));
-      return EXIT_FAILURE;
-    }
+        if (read_res < 0) {
+            (void)fprintf(stderr, "Error: %s\n", modbus_strerror(errno));
+            return EXIT_FAILURE;
+        }
 
-    int result = print(read_regs, n_registers);
-    if (EXIT_SUCCESS != result) return result;
-  } while (n_read_registers < n_registers);
+        int result = print(read_regs, n_registers);
+        if (EXIT_SUCCESS != result) return result;
+    } while (n_read_registers < n_registers);
 
-  return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
